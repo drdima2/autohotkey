f45ad17c2fee0bf58153bd01c2fedf7a9f9badab
drdima <drdima2@gmail.com>
2023-07-02 08:35:48 -0500

    add alt+d send date and time add AutoHotkey64v2.exe, but not use it yet

--- autohokey.bat
+++ autohokey.bat
@@ -1,3 +1,4 @@
 @echo off
 REM create a shortcut to this file, and move to startup folder in start menu
 start AutoHotkeyU64.exe vlc_scroll_lock2.ahk
+rem start AutoHotkey64v2.exe vlc_scroll_lock2.ahk
--- vlc_scroll_lock2.ahk
+++ vlc_scroll_lock2.ahk
@@ -118,6 +118,10 @@ $Esc::
 
 
 
+;---------------- function key for date -------------
+!d::
+    Send, %A_YYYY%-%A_MM%-%A_DD% %A_Hour%:%A_Min%{Space} ; press ALT+d
+
 
 
 
